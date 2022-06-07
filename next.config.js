81ac973bdbcaf15f4e019f1b22743cdb68f903b8
Yanreddddd <johnredkristian.demafeliz@gmail.com>
2022-06-07 22:55:09 +0800

    fix!

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY}
+  env: {NEXT_PUBLIC_STRIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY},
 }
 
 module.exports = nextConfig
