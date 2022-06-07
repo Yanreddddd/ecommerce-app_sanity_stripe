ec06a8a3be5cab2e2aad6484d71a73cd5a7ff02c
Yanreddddd <johnredkristian.demafeliz@gmail.com>
2022-06-07 22:57:02 +0800

    please fix!

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,6 +1,4 @@
-import Stripe from 'stripe';
-
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
