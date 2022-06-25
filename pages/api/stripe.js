dffbf79b8319f9989fbc29d92365e6908e3815ce
Yanreddddd <johnredkristian.demafeliz@gmail.com>
2022-06-25 21:20:39 +0800

    pls work

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,44 +1,49 @@
-import Stripe from 'stripe';
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
-        const params = {
-            submit_type: 'pay',
-            mode: 'payment',
-            payment_method: 'pm_card_visa',
-            payment_method_types: ['card'],
-            billing_address_collection: 'auto',
-            shipping_options: [
-                { shipping_rate: 'shr_1L7lOAGJrHmPhQ9hT66E6mQz'},
-                { shipping_rate: 'shr_1L7lPYGJrHmPhQ9hKuNLzmq6'},
-                { shipping_rate: 'shr_1L7lQFGJrHmPhQ9hqp1bZtBX'},
-            ],
-            line_items: req.body.map((item) => {
-                const img = item.image[0].asset._ref;
-                const newImage = img.replace('image-', 'https://cdn.sanity.io/images/1aegqt91/production/').replace('-webp', '.webp');
-                
-                return {
-                    price_data: { 
-                        currency: 'eur',
-                        product_data: {
-                            name: item.name,
-                            images: [newImage],
-                        },
-                        unit_amount: item.price * 100
-                     },
-                     adjustable_quantity: {
-                         enabled: true,
-                         minimum: 1,
-                     },
-                     quantity: item.quantity
-                }
-            }),
-            success_url: `${req.headers.origin}/success`,
-            cancel_url: `${req.headers.origin}/`,
-          }
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method: "pm_card_visa",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1L7lOAGJrHmPhQ9hT66E6mQz" },
+          { shipping_rate: "shr_1L7lPYGJrHmPhQ9hKuNLzmq6" },
+          { shipping_rate: "shr_1L7lQFGJrHmPhQ9hqp1bZtBX" },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/1aegqt91/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "eur",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/`,
+      };
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
 
@@ -47,7 +52,7 @@ export default async function handler(req, res) {
       return res.status(err.statusCode || 500).json(err.message);
     }
   } else {
-      res.setHeader('Allow', 'POST');
- 
+    res.setHeader("Allow", "POST");
+    return res.status(405).end("Method Not Allowed");
   }
 }
